9242fea19f3b2ef9ecb38bbb2edb6512aeb46589
Senal Fernando <166195579+SenalFernando712@users.noreply.github.com>
2024-04-20 08:29:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,11 @@ import streamlit as st
 # Centered title using Markdown syntax
 st.markdown("<h1 style='text-align: center;'>Enter Your Credentials</h1>", unsafe_allow_html=True)
 
-# Create two columns layout
-col1, col2 = st.coloums(2)
-
 # Username input field
-with col1:
-    username = st.text_input("Username")
+username = st.text_input("Username")
 
 # Password input field
-with col2:
-    password = st.text_input("Password", type="password")
+password = st.text_input("Password", type="password")
 
 
 
