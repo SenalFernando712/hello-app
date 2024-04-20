8ffa9c28222139a0cb1f8121ec6fa4c095ded9a6
Senal Fernando <166195579+SenalFernando712@users.noreply.github.com>
2024-04-20 08:22:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,9 @@ import streamlit as st
 # Centered title using Markdown syntax
 st.markdown("<h1 style='text-align: center;'>Welcome to BCI Choir Finance App </h1>", unsafe_allow_html=True)
 
-st.write('Hello World!')
-st.write('Hi There im editing')
+# Username input field
+username = st.text_input("Username")
+
+# Password input field
+password = st.text_input("Password", type="password")
+
