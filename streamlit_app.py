ef5b48e83612959ec21d5738b020f5dce7404d2a
Senal Fernando <166195579+SenalFernando712@users.noreply.github.com>
2024-04-20 08:35:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit as st
 
 # Centered title using Markdown syntax
 st.markdown("<h1 style='text-align: center;'>Welcome to BCI Choir Finance App</h1>", unsafe_allow_html=True)
+
+# Title for credentials
 st.markdown("<h3 style='text-align: left;'>Enter Your Credentials</h3>", unsafe_allow_html=True)
+
 # Username input field
 username = st.text_input("Username")
 
@@ -13,9 +16,6 @@ password = st.text_input("Password", type="password")
 if st.button("Sign In"):
     # Your sign-in logic goes here
     st.write("Signing in...")
-    if (username == 'Senal') AND (password == 'S3N4L') :
-        st.write('Done')
-    
-
-
-
+    if (username == 'Senal') and (password == 'S3N4L'):
+        # If credentials are correct, show the "Hi" page
+        st.markdown("<h1 style='text-align: center;'>Hi</h1>", unsafe_allow_html=True)
