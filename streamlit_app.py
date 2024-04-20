28a6dd9a85c76b8ebe94ca4e7b95052a8aff7010
Senal Fernando <166195579+SenalFernando712@users.noreply.github.com>
2024-04-20 08:26:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,17 @@
 import streamlit as st
 
-# Centered title using Markdown syntax
-st.markdown("<h1 style='text-align: center;'>Welcome to BCI Choir Finance App </h1>", unsafe_allow_html=True)
+# Create two columns layout
+col1, col2 = st.rows(2)
 
-# Username input field with reduced size and centered
-st.write('<style>input[type="text"] { width: 200px; text-align: center; }</style>', unsafe_allow_html=True)
-username = st.text_input("Username")
+# Username input field
+with col1:
+    username = st.text_input("Username")
 
-# Password input field with reduced size and centered
-st.write('<style>input[type="password"] { width: 200px; text-align: center; }</style>', unsafe_allow_html=True)
-password = st.text_input("Password", type="password")
+# Password input field
+with col2:
+    password = st.text_input("Password", type="password")
+
+st.write('Hello World!')
+st.write('Hi There im editing')
 
 
