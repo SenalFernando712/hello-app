c7915f23b5f064fa7218881b86e36d76b3e8aae0
Senal Fernando <166195579+SenalFernando712@users.noreply.github.com>
2024-04-20 08:20:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Centered title using Markdown syntax
-st.markdown("<h1 style='text-align: center;'>Testing 123</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Welcome to BCI Choir Finance App </h1>", unsafe_allow_html=True)
 
 st.write('Hello World!')
 st.write('Hi There im editing')
